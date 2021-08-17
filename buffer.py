d4869c4f2dac09a3e1c89339a6ebb65b884e251d
Andy Stewart <lazycat.manatee@gmail.com>
2021-08-17 10:29:24 +0800

    Remove unused argument.

--- buffer.py
+++ buffer.py
@@ -31,9 +31,7 @@ class AppBuffer(Buffer):
     def __init__(self, buffer_id, url, arguments):
         Buffer.__init__(self, buffer_id, url, arguments, False)
 
-        self.add_widget(AirShareWidget(url,
-                                       get_emacs_var("eaf-emacs-theme-background-color"),
-                                       get_emacs_var("eaf-emacs-theme-foreground-color")))
+        self.add_widget(AirShareWidget(url, get_emacs_var("eaf-emacs-theme-foreground-color")))
 
 class Image(qrcode.image.base.BaseImage):
     def __init__(self, border, width, box_size):
@@ -59,7 +57,7 @@ class Image(qrcode.image.base.BaseImage):
         pass
 
 class AirShareWidget(QWidget):
-    def __init__(self, url, background_color, foreground_color):
+    def __init__(self, url, foreground_color):
         QWidget.__init__(self)
         self.setStyleSheet("background-color: transparent;")
 
